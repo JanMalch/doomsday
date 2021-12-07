7a3c5d50ad359c0aa4fe4dbf2897a5ea309c7623
JanMalch <25508038+JanMalch@users.noreply.github.com>
2021-12-07 21:58:58 +0100

    build: fix publicPath for CI builds

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,14 @@
 const PrerenderSpaPlugin = require("prerender-spa-plugin");
 const path = require("path");
 
+const isCI = process.env.CI === 'true';
+
+if (isCI) {
+  console.log('CI detected.');
+}
+
 module.exports = {
+  publicPath: isCI ? '/doomsday/' : '/',
   configureWebpack: (config) => {
     if (process.env.NODE_ENV !== "production") {
       return;
